56d4bd579f86ca8f63d3287bdf2e84de27e3de4b
Vitor Walendowsky <vitor.walendowsky@gmail.com>
2026-01-19 20:50:28 -0300

    criterios do projeto

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Teste
+
+
+# 4 crus (2 por aluno)
+
+#Temas:
+# - Monitor ( Tamanho x"- Frequencia Hz - Preco R$ - Loja)
+# - Notebook ( Tamanho Tela" - Memoria RAM GB - Preco R$ - Loja - placa de video - marca da placa de video - RGB S/N - peso kg - SSD S/N)
